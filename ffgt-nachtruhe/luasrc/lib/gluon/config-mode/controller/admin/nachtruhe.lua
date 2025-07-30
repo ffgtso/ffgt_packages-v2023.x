e32d3ede1772aef1b1f75f32ee4b72fccfa7871f
wusel <wusel+src@uu.org>
2025-07-30 02:12:46 +0200

    Fixes

--- ffgt-nachtruhe/luasrc/lib/gluon/config-mode/controller/admin/nachtruhe.lua
+++ ffgt-nachtruhe/luasrc/lib/gluon/config-mode/controller/admin/nachtruhe.lua
@@ -1,4 +1,5 @@
 local wireless = require 'gluon.wireless'
+local uci = require("simple-uci").cursor()
 package 'ffgt-nachtruhe'
 
 if wireless.device_uses_wlan(uci) then
--- ffgt-ssid-changer/luasrc/lib/gluon/config-mode/controller/admin/ssid-changer.lua
+++ ffgt-ssid-changer/luasrc/lib/gluon/config-mode/controller/admin/ssid-changer.lua
@@ -1,4 +1,5 @@
 local wireless = require 'gluon.wireless'
+local uci = require("simple-uci").cursor()
 
 if wireless.device_uses_wlan(uci) then
     entry({"admin", "ssid-changer"}, model("admin/ssid-changer"), _("Offline-SSID"), 35)
